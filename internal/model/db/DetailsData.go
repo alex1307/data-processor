2b7cd1b89df0295bdb0d41f9a640cb4cd8b12d66
Alexander Todorov <e-alexander.todorov@smart.com>
2024-02-08 16:22:49 +0200

    Processing data with kafka.

--- internal/model/db/DetailsData.go
+++ internal/model/db/DetailsData.go
@@ -6,8 +6,8 @@ type DetailsData struct {
 	ID         uint64    `gorm:"column:id; primary_key:yes; type: integer; auto_increment:true"`
 	ADVERT_ID  string    `gorm:"column:advert_id; type: character varying(36); not null"`
 	Source     string    `gorm:"column:source; type: character varying(25); not null"`
-	Phone      string    `gorm:"column:phone; type: character varying(25); not null"`
-	Location   string    `gorm:"column:location; type: character varying(50); not null"`
+	Phone      string    `gorm:"column:phone; type: character varying(50); not null"`
+	Location   string    `gorm:"column:location; type: character varying(100); not null"`
 	ViewCount  uint32    `gorm:"column:view_count; default: 0"`
 	Equipment  uint64    `gorm:"column:equipment; default: 0"`
 	SellerName string    `gorm:"column:seller_name; type: character varying(50); not null"`
